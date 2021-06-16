10728a402c9003b89657625da513668e70804103
Anle101 <46081192+Anle101@users.noreply.github.com>
2021-06-16 18:15:06 -0400

    Implemented front-page of application (Version 0.1)
    Implemented the front-page of the application. Use of React and the react-spring library for animations.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { useSpring, animated } from 'react-spring';
+
 import './App.css';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
 
+  const fade = useSpring({ 
+    to: {opacity: 1 }, 
+    from: {opacity: 0},
+    delay:300,
+    
+    config: {duration: 1000},
+  });
+
+  const fadehalf = useSpring({ 
+    to: {opacity: 0.5 }, 
+    from: {opacity: 0},
+    delay:300,
+    
+    config: {duration: 1000},
+  });
+
+  const translate = useSpring({ 
+    to: {opacity: 1 ,marginTop:0}, 
+    from: {opacity: 0, marginTop:-200},
+    
+    
+    config: {mass: 1, frequency: 1, damping: 1},
+  });
+
+  const rotation = useSpring({
+    to:{rotateZ:0}, 
+    from:{rotateZ:360},
+    loop: true,
+
+    config: {duration:10000},
+  });
   useEffect(() => {
     fetch('/time').then(response => response.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current Time is {currentTime}.</p>
-      </header>
+    <div className= "App">
+
+      <animated.h1 style = {translate}> Welcome to Topic Tracking </animated.h1>
+      <animated.a style = {fade} className="button"> Let's begin </animated.a>
+      <animated.div style={fadehalf} className="circle"></animated.div>
+      <animated.div style={fadehalf} className="larger-circle"></animated.div>
+      <animated.div style={rotation} className="triangle"></animated.div>
     </div>
   );
 }
