d8981dce3a2a7855ef80f1420246a88318a6a75a
slickLink <lincoln.dube@outlook.com>
2021-07-10 02:24:26 -0400

    tweets by date range function
    api.py - gets input from frontend
    twitter_converter.py - adds a class for pulling tweets from database

--- api/api.py
+++ api/api.py
@@ -1,14 +1,30 @@
 import time
 from flask import Flask, jsonify, request
+from twitter_converter import TweetFactory
 
 
 app = Flask(__name__)
+DAL = TweetFactory()
 
 sqlstatement = ''
 
-@app.route('/time')
+@app.route('/time', methods=['GET'])
 def get_current_time():
-    return {'time': time.time() }
+    # check id start_date and end_date were provided in url
+    if 'start_date' not in request.args:
+        return {"error": "missing start_date argument"}
+
+    if 'end_date' not in request.args:
+        return {"error": "missing end_date argument"}
+    
+    # retrieve tweets in date range
+    start_date = request.args['start_date']
+    end_date = request.args['end_date']
+    results = DAL.tweets_by_date_range(start_date, end_date)
+    data = {}
+    for x in results:
+        data[x[0]] = x[1]
+    return data
 
 
 @app.route('/add_sqlstatement', methods=['POST'])
---
+++ api/twitter_converter.py
@@ -0,0 +1,62 @@
+from db_conn import Database
+from dateutil.parser import parse
+
+class TweetFactory:
+    """
+    This class is used to retrieve tweets from the database based on given filters
+    This is a DAL class
+    ...
+
+    Attributes
+    ----------
+    db: db class 
+        This is the database
+
+    Methods
+    -------
+    tweets_by_date_range(start_date, end_date)
+    tweets_by_date(date)
+    tweets_by_user(user_id)
+
+    """
+    def __init__(self):
+        self.db = Database()
+    
+    def is_validate_date(self,date):
+        """
+        checks a given string for validate date 
+        on success: True
+        on failure: False
+        """
+        try:
+            parse(date)
+            return True
+        except ValueError:
+            return False
+
+    def tweets_by_date_range(self, start_date, end_date):
+        """
+        on success: json of tweets in daterange from db
+        on failure: json with error message
+        """
+        # validate input dates
+        if (not self.is_validate_date(start_date)) or (not self.is_validate_date(end_date)):
+            return {"error": "invalid date"}
+        
+        # validate date range
+        if (parse(start_date) > parse(end_date)):
+            return {"error": "invalid date range"}
+        
+        # retrieve tweets in date range
+
+        print(f"start_date: {start_date}")
+        print(f"end_date: {end_date}")
+
+        # TODO: error handling 
+        # TODO: add a fetchquery data function to DATABASE
+        query = f'SELECT tweetId, tweetText FROM tweets_table WHERE tweetDate BETWEEN {start_date} and \'{end_date} 23:59:59\''
+        dbcursor = self.db.get_connection().cursor()
+        dbcursor.execute(query)
+        data = dbcursor.fetchall()
+        return data
+        
