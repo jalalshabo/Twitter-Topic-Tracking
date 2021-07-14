e0da9bcff332396bbabc3b7b62f00b6923bf36ab
slickLink <lincoln.dube@outlook.com>
2021-07-14 18:05:44 -0400

    Infer topics from new documents
    1 - extends custom stopwords
    2 - refined preprocessing using regular expression
    3 - function infer_topics, infers topics of new documents from
    previously trained lda model

--- api/lda.py
+++ api/lda.py
@@ -1,5 +1,6 @@
 from os import replace
 from pprint import pprint
+from gensim.corpora import dictionary
 
 import nltk
 from nltk import corpus
@@ -46,22 +47,24 @@ class Lda:
         run_lda(tweet_corpus)
     """
     def __init__(self):
-        self.stopwords = None
-        self.npl = spacy.load('en_core_web_sm')
         self.corpus = None
         self.processed_corpus = None
+        self.trained_model = None
+        self.dictionary = None
+        self.stopwords = stopwords.words('english')
+        self.npl = spacy.load('en_core_web_sm')
         self.num_topics = 10
         self.id2word = None
-        self.workers = multiprocessing.cpu_count()
+        self.workers = 4
         self.chunksize = 2000
-        self.passes=50
+        self.passes=20
         self.batch=True
         self.alpha='symmetric'
         self.eta=None
         self.decay=0.5
         self.offset=1.0
-        self.eval_every=None
-        self.iterations=50
+        self.eval_every=1
+        self.iterations=400
         self.gamma_threshold=0.001
         self.random_state=100
         pass
@@ -70,6 +73,12 @@ class Lda:
         if type(_list) is list:
             print("{}\n-------------------------".format(msg))
             print(*_list, sep='\n')
+    
+    def visualize_model(self, corpus):
+        output_name = 'output'
+        vis_model = pyLDAvis.gensim_models.prepare(self.trained_model, corpus, self.dictionary)
+        pyLDAvis.save_html(vis_model, f'{output_name}.html')
+        return
 
     # preprocessing library that removes links, new lines, emojis and more, augments the list sent to the function
     def preprocess_tweets(self, tweet_corpus):
@@ -78,8 +87,8 @@ class Lda:
         # lower each tweet
         tweets = [ tweet.lower() for tweet in tweets]
         # remove extra garbage
-        tweets = [re.sub("\'","", tweet) for tweet in tweets]
-        tweets = [re.sub("[\.?!,:-;\"\[\]]|\\\\n", "", tweet) for tweet in tweets]
+        tweets = [re.sub("\'"," ", tweet) for tweet in tweets]
+        tweets = [re.sub("[/\^&\*\\\.?!,:\-;\"\[\]]|\\\\n", " ", tweet) for tweet in tweets]
         return tweets
 
     # Removing empty lists with filter, augments list sent
@@ -98,8 +107,27 @@ class Lda:
         # remove words that only contain one character
         tweets = [[ token for token in tweet if len(token) > 1 ] for tweet in tweets]
         # remove stop words
-        custom_stopwords = ['amp', '&amp', '\\c200c', '\\c200d']
-        self.stopwords = self.npl.Defaults.stop_words.union(custom_stopwords, stopwords.words('english'))
+        custom_stopwords = ['amp', '&amp', '\\c200c', '\\c200d', 'lol', 'able', 'abst', 'accordance', 'according', 'accodringly', 'act', 'actually',
+        'added', 'adj', 'affected', 'affecting', 'affects', 'ah', 'all', 'almost', 'announce', 'anybody', 'anymore', 'apparently', 'approximately',
+        'arent', 'arise', 'aside', 'ask', 'asking', 'at', 'available', 'away','awful', 'came', 'couldnt', 'cant', 'cause', 'causes', 'certain', 'certainly',
+        'co', 'com', 'come', 'comes', 'contain', 'containing', 'contains', 'different', 'downwards', 'ed', 'edu', 'effect', 'end', 'ending', 'especially',
+        'et', 'etc', 'everybody', 'ex', 'except', 'far', 'ff', 'fifth', 'fix','followed', 'following', 'follows', 'forth', 'found', 'futhermore',
+        'gave', 'gets', 'getting', 'given', 'gives', 'giving', 'goes', 'gone', 'got', 'gotten', 'hadnt', 'happens', 'hardly', 'hence', 'id', 'ie', 'ill', 'indeed',
+        'instead', 'important', 'importance', 'immediately', 'immediate', 'inc', 'into', 'ive', 'im', 'keeps', 'kept', 'kg', 'km', 'know', 'known', 'knows',
+        'largely', 'lest', 'let', 'lets', 'like', 'lately', 'like', 'liked', 'likely', 'look', 'ltd', 'maybe', 'mean', 'means', 'meantime', 'meanwhile', 'merely', 'mg',
+        'mr', 'mrs', 'na', 'nay', 'near', 'nearly', 'necessary', 'necessarily', 'need', 'needs', 'new', 'ninety', 'non', 'normally', 'nos', 'noted',
+        'obtain', 'obtained', 'obviously', 'oh', 'ok', 'okay', 'old', 'outside', 'overall', 'page', 'pages', 'particular', 'particularly', 'placed',
+        'plus', 'poorly', 'possible', 'possibly', 'potentially', 'pp', 'previously', 'primarily', 'promptly', 'proud', 'provides', 'que', 'quickly', 'quite',
+        'regardless', 'regards', 'related', 'right', 'said', 'saw', 'saying', 'sec', 'seeing', 'self', 'several', 'shall', 'shell', 'shouldnt', 'shouldve',
+        'showed', 'shown', 'significant', 'significantly', 'similar', 'similarly', 'since', 'slightly', 'soon', 'still', 'sure', 'sup', 'such', 'stop',
+        'taken', 'taking', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thatll', 'thatve', 'therell', 'theres', 'theyd', 'theyll', 'theyre', 'theyd', 
+        'think', 'this', 'those', 'thru', 'thus', 'til', 'tip', 'took', 'toward', 'towards', 'tried', 'tries', 'try', 'trying', 'twice', 'two', 'un', 'unfortunately',
+        'unlike','unlikely', 'until', 'unto', 'up', 'upon', 'ups', 'useful', 'usefully', 'usefulness', 'us', 'using', 'uses', 'usually', 'vis', 'vol',
+        'vols', 'vs', 'want', 'wants', 'way', 'wed', 'well', 'whats', 'when', 'whence', 'whos', 'whose', 'widely', 'wouldnt', 'www', 'yes', 'youd', 'youll', 'youre',
+        'zero', 'z']
+        self.stopwords = self.npl.Defaults.stop_words.union(custom_stopwords, self.stopwords)
+        print(sorted(self.stopwords))
+        print(type(self.stopwords))
         tweets = [[token for token in tweet if not token in self.stopwords] for tweet in tweets]
         return tweets
 
@@ -113,24 +141,39 @@ class Lda:
         lemmatizer = WordNetLemmatizer()
         tweets = [[lemmatizer.lemmatize(token) for token in tweet] for tweet in tweet_corpus ]
         return tweets
+    
+    def infer_topics(self, unseen_corpus):
+        # preprocess new documents
+        corpus = self.preprocess_tweets(unseen_corpus)
+        corpus = self.tokenize(corpus)
+        corpus = self.remove_empty(corpus)
+        corpus = self.lem_data(corpus)
 
-    # run test
-    def run_lda(self, tweet_corpus):
+        temp_corpus = [ self.dictionary.doc2bow(document) for document in corpus]
+        temp = self.trained_model[temp_corpus]
+        for topic in temp:
+            print(topic)
+        return temp
+
+    # train new corpus
+    def train_lda(self, tweet_corpus):
+        # self.print_list(tweet_corpus, "before preprocess")
+        # preprocess tweet_corpus 
         corpus = self.preprocess_tweets(tweet_corpus)
         corpus = self.tokenize(corpus)
         corpus = self.remove_empty(corpus)
         corpus = self.lem_data(corpus)
 
         #Create a dictionary representation of the documents
-        dictionary = Corpora.Dictionary(corpus)
+        self.dictionary = Corpora.Dictionary(corpus)
         # filter out words that occur in less than 1 document or more than 50% of the corpus
-        dictionary.filter_extremes(no_below=2, no_above=0.65)
-        # create a bag-of-words representation of the corpus
-        self.corpus = [ dictionary.doc2bow(document) for document in corpus]
+        # self.dictionary.filter_extremes(no_below=2, no_above=0.65)
+        # # create a bag-of-words representation of the corpus
+        self.corpus = [ self.dictionary.doc2bow(document) for document in corpus]
         ### training the LDA model
         # make a index to word dictionary
-        temp = dictionary[0] # load the dict to memory
-        id_word = dictionary.id2token
+        temp = self.dictionary[0] # load the dict to memory
+        id_word = self.dictionary.id2token
 
         # singlecore implementation
         # lda_model = gensim.models.ldamodel.LdaModel(
@@ -148,7 +191,7 @@ class Lda:
         #     per_word_topics=True
         # )
         # mulitcore implementation
-        lda_model = gensim.models.LdaMulticore(
+        self.trained_model = gensim.models.LdaMulticore(
             corpus=self.corpus,
             num_topics=self.num_topics,
             id2word=id_word,
@@ -165,9 +208,5 @@ class Lda:
             gamma_threshold=self.gamma_threshold,
             random_state=self.random_state
         )
-        output_name = 'output'
-        vis_model = pyLDAvis.gensim_models.prepare(lda_model, self.corpus, dictionary)
-        pyLDAvis.save_html(vis_model, f'{output_name}.html')
-
-        return tweet_corpus
+        return corpus
 
